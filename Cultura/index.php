3d1982ba040fbad9bd7b9a4900262a529e9be80b
LuanaVans <lunash_@hotmail.com>
2025-02-20 14:49:48 +0100

    corregido

--- Cultura/index.php
+++ Cultura/index.php
@@ -24,19 +24,25 @@ $resultado_array = mysqli_query($conn, $sql);
 if (mysqli_num_rows($resultado_array) > 0) {
     echo "<ul>";
     while ($row = mysqli_fetch_assoc($resultado_array)) {
-        echo "<div class='container'>
+        echo "<li class='container'>
+        <div class='contenedor'>
+        <img src='{$row['img_url']}' alt='Imagen del evento'class='evento'>
+        </div>
+        <div class='contenedor'>
         <h2>{$row['nombre']}</h2>
-        <img src='{$row['img_url']}' alt='Imagen del evento'>
-        <p>{$row['tipo']}</p>
-        <p>{$row['descripcion']}</p>
-        <p>{$row['fecha_inicio']}</p>
-        <p>{$row['fecha_fin']}</p>
-        <p>{$row['hora']}</p>
-        <p>{$row['lugar']}</p>
-        <p>{$row['direccion']}</p>
-        <p>{$row['precio']}</p>
-        </li>
-        </div>";
+            <p>Tipo: {$row['tipo']}</p>
+            <p>Descripcion: {$row['descripcion']}</p>
+            <p>Fecha Inicio: ".date("d M Y", strtotime($row['fecha_inicio']))."</p>
+            <p>Fecha Fin: ".date("d M Y", strtotime($row['fecha_inicio']))."</p>
+            <p>Hora: ".date('H:i', strtotime($row['hora']))."</p>
+            <p>Lugar: {$row['lugar']}</p>
+            <p>Dirección: {$row['direccion']}</p>
+            <p>Precio: {$row['precio']}€</p>
+        </div>
+        </li>";
+    
+
+
     }
     echo "</ul>";
 }
